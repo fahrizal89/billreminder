6390c31ac90a655ec6c28da6fb6454709d0e8e5a
Fahrizal Sentosa <fahrizal.sentosa@dana.id>
2022-10-31 00:19:33 +0700

    set default notification at 9.00 AM

--- app/src/main/java/id/fahrizal/billreminder/domain/usecase/SaveBill.kt
+++ app/src/main/java/id/fahrizal/billreminder/domain/usecase/SaveBill.kt
@@ -28,12 +28,15 @@ class SaveBill @Inject constructor(
         year: Int = 1
     ): ArrayList<Reminder> {
         val reminders = ArrayList<Reminder>()
-        val firstReminder = DateUtil.getDate(0, 15, 0, bill.dayInMonth)
+        val firstReminder =
+            DateUtil.getDate(NOTIFY_HOUR, NOTIFY_MINUTE, NOTIFY_SECOND, bill.dayInMonth)
         for (i: Int in 0 until (year * 12)) {
             val time = DateUtil.plusMonth(firstReminder.time, i)
             val body = context.getString(
                 R.string.notification_message,
-                bill.dayInMonth.toString() + " " + bill.name +" " + CurrencyUtil.getRupiahAmount(bill.amount)
+                bill.dayInMonth.toString() + " " + bill.name + " " + CurrencyUtil.getRupiahAmount(
+                    bill.amount
+                )
             )
             Timber.d("fahrizal SaveBill at " + DateUtil.getTimeInString(time))
             val reminder = Reminder(billId, body, time)
@@ -41,5 +44,12 @@ class SaveBill @Inject constructor(
         }
 
         return reminders
+    }
+
+    companion object {
+
+        private const val NOTIFY_HOUR = 9
+        private const val NOTIFY_MINUTE = 0
+        private const val NOTIFY_SECOND = 0
     }
 }
